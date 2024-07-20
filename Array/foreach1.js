1995d3ae33f6f59c2e3be9dd2cc7d195a41c4dc0
Gabriel dos Anjos Senra <fullstackgabe@Gabriels-MacBook-Air.local>
2024-07-20 11:19:21 -0300

    foreach1

---
+++ Array/foreach1.js
@@ -0,0 +1,13 @@
+const aprovados = ["Agatha", "Aldo", "Daniel", "Raquel"]
+
+//callback
+aprovados.forEach(function(nome, indice, array, x) {
+    console.log(`${indice + 1}) ${nome}`)
+    console.log(array)
+    console.log(x)
+})
+
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovados.forEach(exibirAprovados)
