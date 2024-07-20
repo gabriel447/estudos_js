c3cc954004f4b8dc12d8e90d00fe7f65ec7b39ce
Gabriel dos Anjos Senra <fullstackgabe@Gabriels-MacBook-Air.local>
2024-07-20 13:51:57 -0300

    concatenando arrays

---
+++ Array/concat.js
@@ -0,0 +1,6 @@
+const frutas = ["Maçã", "Banana", "Laranja"];
+const legumes = ["Cenoura", "Abobrinha", "Brócolis"];
+
+const listaComestiveis = frutas.concat(legumes);
+
+console.log(listaComestiveis);
