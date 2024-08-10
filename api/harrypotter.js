57200bc2f56e90646ae7785daf5ddbd5aa6a4f6c
Gabriel447 <gabriel_447@live.com>
2024-08-10 19:51:09 -0300

    implementando catch api

--- api/harrypotter.js
+++ api/harrypotter.js
@@ -1,6 +1,9 @@
 async function getData(endpoint) {
   try {
-    const response = await fetch(`https://hp-api.onrender.com/api/${endpoint}`);
+    const response = await fetch(`https://hpa-api.onrender.com/api/${endpoint}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     const data = await response.json();
     return data;
   } catch (e) {
@@ -8,6 +11,10 @@ async function getData(endpoint) {
   }
 }
 
-getData("characters").then((data) => {
-  data.forEach((element) => console.log(element.name));
-});
+getData("characters")
+  .then((data) => {
+    data.forEach((element) => console.log(element.name));
+  })
+  .catch((error) => {
+    console.error("Erro ao buscar os dados:", error);
+  });
--- api/rickandmorty.js
+++ api/rickandmorty.js
@@ -1,6 +1,9 @@
 async function getData(endpoint) {
   try {
     const response = await fetch(`https://rickandmortyapi.com/api/${endpoint}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     const data = await response.json();
     return data;
   } catch (e) {
@@ -8,6 +11,10 @@ async function getData(endpoint) {
   }
 }
 
-getData("character").then((data) => {
-  data.results.forEach((element) => console.log(element.name));
-});
+getData("character")
+  .then((data) => {
+    data.results.forEach((element) => console.log(element.name));
+  })
+  .catch((error) => {
+    console.error("Erro ao buscar os dados:", error);
+  });
